1d2bd77c441a7373e022922496c250b30f786aae
Anushka Srivastava <anusri1904@gmail.com>
2024-06-10 18:02:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,5 +28,5 @@ def get_sum():
         return jsonify({'error': 'Error from API Gateway'}), response.status_code
 
 if __name__ == '__main__':
-     app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
     app.run(debug=True)
